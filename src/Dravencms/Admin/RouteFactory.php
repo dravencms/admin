4a2dc1fb8d2594d6361059e73247cd1369616bb9
Adam Schubert <adam.schubert@sg1-game.net>
2016-11-27 19:58:17 +0100

    Added router

---
+++ src/Dravencms/Admin/RouteFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Dravencms\Admin;
+
+use Nette\Application\Routers\Route;
+use Nette\Application\Routers\RouteList;
+
+/**
+ * Copyright (C) 2016 Adam Schubert <adam.schubert@sg1-game.net>.
+ */
+class RouteFactory
+{
+    /**
+     * @return \Nette\Application\IRouter
+     */
+    public function createRouter()
+    {
+        $router = new RouteList();
+
+
+        $router[] = $admin = new RouteList('Admin');
+        $admin[] = new Route('admin/index.php', 'Homepage:Homepage:default', Route::ONE_WAY);
+        $admin[] = new Route('admin/[<locale [a-z]{2}>/]<presenter>/<action>[/<id [0-9]+>]', 'Homepage:Homepage:default');
+
+        return $router;
+    }
+}
