27764581b69d5e81fdbc2995c3d8e591c3b78ab8
Adam Schubert <adam.schubert@sg1-game.net>
2021-10-14 21:39:51 +0200

    feat(Template): Call parent formatLayoutTemplateFiles

--- src/Dravencms/AdminModule/BasePresenter.php
+++ src/Dravencms/AdminModule/BasePresenter.php
@@ -84,25 +84,9 @@ abstract class BasePresenter extends \Dravencms\BasePresenter
      */
     public function formatLayoutTemplateFiles(): array
     {
-        $name = $this->getName();
-        $presenter = substr($name, strrpos(':' . $name, ':'));
-        $className = trim(str_replace($presenter . 'Presenter', '', get_class($this)), '\\');
-        $exploded = explode('\\', $className);
-        $moduleName = str_replace('Module', '', end($exploded));
-        $layout = $this->layout ? $this->layout : 'layout';
-        $dir = dirname($this->getReflection()->getFileName());
-        $dir = is_dir("$dir/templates") ? $dir : dirname($dir);
-        $list = [
-            "$dir/templates/$moduleName/$presenter/@$layout.latte",
-            "$dir/templates/$moduleName/$presenter.@$layout.latte",
-        ];
-        do {
-            $list[] = "$dir/templates/@$layout.latte";
-            $dir = dirname($dir);
-        } while ($dir && ($name = substr($name, 0, strrpos($name, ':'))));
-
+        $list = parent::formatLayoutTemplateFiles();
+        $layout = $this->getLayoutName();
         $list[] = __DIR__."/templates/@$layout.latte";
-        
         return $list;
     }
 }
